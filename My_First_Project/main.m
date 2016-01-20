cf124251dee94d6d6828e5f14760a1c1bb4cb272
Tammy Tyberg <tammy.tyberg@smallplanet.com>
2016-01-20 16:09:29 -0500

    Initial Commit

---
+++ My_First_Project/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  My_First_Project
+//
+//  Created by Tammy Tyberg on 1/20/16.
+//  Copyright © 2016 Tammy Tyberg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
